58a4bb39083d73d7052b57ca63aad856ac1939bb
itzmevicky <104923165+itzmevicky@users.noreply.github.com>
2022-06-22 10:50:32 +0530

    Update views.py

--- Destinations/views.py
+++ Destinations/views.py
@@ -3,7 +3,5 @@ from Destinations.models import Destinations,Destinations_States
 # Create your views here.
 def index(request):
     states = Destinations_States.objects.filter(destination__division_Name = 'East') 
-    context = {
-        'names' : states
-    }
-    return render(request,'Home.html',context)
+    
+    return render(request,'Home.html',{'states':states})
